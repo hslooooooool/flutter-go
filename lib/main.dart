6a2fb4aba0c6171bbef8f24a63b59b552b665f43
华清松 <821034742@qq.com>
2019-04-04 18:31:53 +0800

    整理代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,37 @@
-import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'routers/routers.dart';
-import 'routers/application.dart';
-
+import 'package:flutter_go/model/search_history.dart';
 import 'package:flutter_go/utils/provider.dart';
 import 'package:flutter_go/utils/shared_preferences.dart';
 import 'package:flutter_go/views/first_page/home.dart';
-import 'package:flutter_go/model/search_history.dart';
-//import 'views/welcome_page/index.dart';
+
+import 'routers/application.dart';
+import 'routers/routers.dart';
+import 'views/welcome_page/index.dart';
 
 const int ThemeColor = 0xFFC91B3A;
 SpUtil sp;
 var db;
 
 class MyApp extends StatelessWidget {
-  MyApp()  {
+  MyApp() {
     final router = new Router();
 
     Routes.configureRoutes(router);
 
     Application.router = router;
   }
+
   showWelcomePage() {
-    // 暂时关掉欢迎介绍
-    return AppPage();
-//    bool showWelcome = sp.getBool(SharedPreferencesKeys.showWelcome);
-//    if (showWelcome == null || showWelcome == true) {
-//      return WelcomePage();
-//    } else {
-//      return AppPage();
-//    }
+    bool showWelcome = sp.getBool(SharedPreferencesKeys.showWelcome);
+    if (showWelcome == null || showWelcome == true) {
+      return WelcomePage();
+    } else {
+      return AppPage();
+    }
   }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Color(0xFFEFEFEF),
         accentColor: Color(0xFF888888),
         textTheme: TextTheme(
-          //设置Material的默认字体样式
+          // 设置Material的默认字体样式
           body1: TextStyle(color: Color(0xFF888888), fontSize: 16.0),
         ),
         iconTheme: IconThemeData(
@@ -49,14 +49,11 @@ class MyApp extends StatelessWidget {
           size: 35.0,
         ),
       ),
-      home: new Scaffold(
-        body: showWelcomePage()
-      ),
+      home: new Scaffold(body: showWelcomePage()),
       onGenerateRoute: Application.router.generator,
     );
   }
 }
-
 
 void main() async {
   final provider = new Provider();
